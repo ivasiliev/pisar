8fa037a9a3cd8b6935472690817941696ed27bfe
Evgeny Leonov <eugene.leonov@merck.com>
2023-05-17 17:18:52 +0200

    attaching input parameters

--- pisar/run_doc_official_proceeding.py
+++ pisar/run_doc_official_proceeding.py
@@ -1,3 +1,5 @@
+import os
+
 from classes.document_in_report import MODEL_PERSONNEL_PATH, MODEL_OUTPUT_FOLDER, MODEL_PERSONS, MODEL_MORPHOLOGY
 from documents.doc_official_proceeding import DocOfficialProceeding
 import pymorphy2
@@ -5,7 +7,10 @@ import sys
 
 if __name__ == '__main__':
 	print("Писарь начинает работу")
-	
+	settings_full_path = str(sys.argv[1])
+	print(f"Файл настроек={settings_full_path}")
+	if not os.path.exists(settings_full_path):
+		print(f"Файл настроек не обнаружен либо недоступен. Выполнение программы прервано.")
 
 	morph = pymorphy2.MorphAnalyzer()
 	data_model = {
