f036871f0ed45237d96cab3648226be6738d9468
Evgeny Leonov <eugene.leonov@merck.com>
2023-06-05 22:40:39 +0200

    Install and Update functionality

--- pisar/install/install-python-libs.bat
+++ pisar/install/install-python-libs.bat
@@ -6,7 +6,5 @@ pip install python-docx
 pip install pymorphy2
 pip install pandas
 pip install numpy
-rem pip install -U pymorphy2-dicts-ru
 pip install pymorphy2-dicts-ru
-rem pip install petrovich
 pip install pytrovich
