a7c77400de215e840b3e52ea55e727644f80237b
Evgeny Leonov <eugene.leonov@merck.com>
2023-06-05 22:52:37 +0200

    runner

--- pisar/gui/main_window.py
+++ pisar/gui/main_window.py
@@ -1,10 +1,12 @@
 import PySimpleGUI as sg
+import subprocess
 
 sg.theme('DarkGreen5')
 
 layout = [[sg.Text('Your typed chars appear here:'), sg.Text(size=(15,1), key='-OUTPUT-')],
           [sg.Input(key='-IN-')],
-          [sg.Button('Show'), sg.Button('Exit')]]
+          [sg.Button('Show'), sg.Button('Exit'), sg.Button(key="updateButton", button_text="Обновить")]
+          ]
 
 window = sg.Window('Pattern 2B', layout)
 
@@ -16,5 +18,9 @@ while True:  # Event Loop
     if event == 'Show':
         # Update the "output" text element to be the value of "input" element
         window['-OUTPUT-'].update(values['-IN-'])
+    if event == 'updateButton':
+        window['-OUTPUT-'].update("Обновление...")
+        subprocess.call(["c:\\Leonov\\pisar\\pisar\\install\\update.bat"])
+        window['-OUTPUT-'].update("Обновление выполнено")
 
 window.close()
