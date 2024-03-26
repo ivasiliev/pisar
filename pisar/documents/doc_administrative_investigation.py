54ed14054b0e3a04589f63624c22c94ae90c3e75
GarmoskaDobra <leonov.eugene@gmail.com>
2024-03-26 21:38:57 +0100

    current progress in Desert Unit

--- pisar/documents/doc_administrative_investigation.py
+++ pisar/documents/doc_administrative_investigation.py
@@ -2,6 +2,7 @@
 # АДМИНИСТРАТИВНОЕ РАССЛЕДОВАНИЕ по факту самовольного оставления части
 from classes.pers_full_name_settings import PersFullNameSettings
 from documents.investigation_prototype import InvestigationPrototype
+from helpers.text_helper import capitalize_first_letter
 
 
 class DocAdministrativeInvestigation(InvestigationPrototype):
@@ -11,16 +12,22 @@ class DocAdministrativeInvestigation(InvestigationPrototype):
 	def get_name_for_file(self):
 		return f"Административное расследование_СОЧ ({self.get_soldier_info().full_name}).docx"
 
+	def get_rank_name(self, declension_type):
+		s_info = self.get_soldier_info()
+		rank = self.get_person_rank(s_info.rank, declension_type)
+		full_name = self.get_person_name_declension(self.get_person_name_short_format_1(s_info.full_name), declension_type)
+		return f"{rank} {full_name}"
+
 	def render(self, custom_margins=None):
+		sold_str = self.get_rank_name(1)
+		sold_str_2 = self.get_rank_name(2)
+
 		self.doc_title = "АДМИНИСТРАТИВНОЕ РАССЛЕДОВАНИЕ"
 		self.doc_about = "по факту самовольного оставления части"
 
 		self.inventory_caption = "административного расследования"
-		rows_content = ["Служебная характеристика", "Справка по форме ГУК МО РФ от 04.12.2006 г.", f"Письмо командования в/ч {self.get_military_unit()} родителям", "Ориентировка", "Копия паспорта", "Копия служебной карточки", "Заключение административного расследования"]
+		rows_content = [f"Медицинская характеристика на {sold_str}", f"Копия служебной карточки на {sold_str}", f"Служебная характеристика на {sold_str}", "Справка по форме ГУК МО РФ от 04.12.2006 г.", "Копия ведомости закрепления оружия 6 СР", "Копия именного списка для вечерних поверок 6 СР", f"Копия ведомости об ознакомлении со статьями УК РФ {sold_str}", f"Выписки из приказов командира войсковой части {self.get_military_unit()} на {sold_str}", f"Акт о невозможности дачи объяснений {sold_str_2}", f"Копия паспорта {sold_str}", "Заключение служебного разбирательства"]
 		self.add_inventory_list(rows_content)
-
-		self.report1_action = "самовольно покинул расположение части, не уведомив вышестоящее командование"
-		self.report1_request = "проведение административного расследования по данному факту"
 
 		self.report3_conclusion = "самовольно покинул расположение части, не уведомив вышестоящее командование"
 
@@ -34,6 +41,7 @@ class DocAdministrativeInvestigation(InvestigationPrototype):
 
 		settings = PersFullNameSettings(2, False, False, True, False, True, False)
 		sold_str = self.get_person_full_str(settings)
+
 		txt = f"3. Передать материалы административного расследования по факту самовольного оставления части {sold_str} в военную прокуратуру г.Донецка для дальнейшего принятия процессуального решения."
 		self.conclusion_punishment_points.append(txt)
 
@@ -49,3 +57,21 @@ class DocAdministrativeInvestigation(InvestigationPrototype):
 		self.conclusion_punishment_points.append(txt)
 
 		super().render()
+
+	def report1_page(self):
+		self.add_paragraph(f"Командиру войсковой части {self.get_military_unit()}", self.align_right_settings)
+		self.add_empty_paragraphs(2)
+		self.add_paragraph("Рапорт", self.align_center_settings)
+		self.add_empty_paragraphs(1)
+		settings = PersFullNameSettings(0, False, False, True, False, False, False)
+		sold_str = self.get_person_full_str(settings)
+		date_str = self.get_date_format_1(self.get_date_of_event())
+		self.add_paragraph(f"Настоящим докладываю, что {sold_str} отсутствовал на военной службе {date_str} на утренней поверке 6 стрелковой роты 2 стрелкового батальона, не уведомив при этом вышестоящее командование.", self.ident_align_justify_settings)
+		settings = PersFullNameSettings(0, False, False, False, False, False, False, address_required=False)
+		sold_str1 = capitalize_first_letter(self.get_person_full_str(settings))
+		settings = PersFullNameSettings(1, False, False, False, False, False, False, address_required=False)
+		sold_str2 = self.get_person_full_str(settings)
+		self.add_paragraph(f"Мной была поставлена задача поисковой группе, которая провела поисковые мероприятия в окрестностях пункта временной дислокации 6 стрелковой роты, а также по адресу прописки, а именно: <АДРЕС ПРОПИСКИ>. {sold_str1} обнаружен не был. Мобильный телефон военнослужащего выключен. В ходе беседы с соседями {sold_str2} получили информацию, что данный военнослужащий по месту прописки не появлялся и где находится им не известно. ", self.ident_align_justify_settings)
+
+		self.add_empty_paragraphs(3)
+		self.officer_report_footer("commander_company")
--- pisar/helpers/text_helper.py
+++ pisar/helpers/text_helper.py
@@ -1,138 +1,138 @@
 def decode_acronyms(morph, position, declension_type):
-	if position is None or len(position) == 0:
-		return ""
-	# pairs text -- index of word for declension
-	acronyms = {
-		"врио": ["временно исполняющий обязанности", 1]
-		, "ср": ["стрелковая рота", -1]
-		, "сб": ["стрелковый батальон", -1]
-	}
+    if position is None or len(position) == 0:
+        return ""
+    # pairs text -- index of word for declension
+    acronyms = {
+        "врио": ["временно исполняющий обязанности", 1]
+        , "ср": ["стрелковая рота", -1]
+        , "сб": ["стрелковый батальон", -1]
+    }
 
-	result = ""
-	words = position.split(" ")
-	for wrd in words:
-		word = wrd.lower()
-		if word in acronyms:
-			word_acr_info = acronyms[word]
-			word_index = word_acr_info[1]
-			# if there is no special index of word, then convert all words
-			words_acr_all = word_acr_info[0].split(" ")
-			if word_index == -1:
-				for words_acr_all_each in words_acr_all:
-					result = result + " " + get_word_declension(morph, words_acr_all_each, 1)
-			else:
-				current_index = 0
-				for words_acr_all_each in words_acr_all:
-					if current_index == word_index:
-						result = result + " " + get_word_declension(morph, words_acr_all_each, declension_type)
-					else:
-						result = result + " " + words_acr_all_each
-					current_index = current_index + 1
-		else:
-			result = result + " " + word
+    result = ""
+    words = position.split(" ")
+    for wrd in words:
+        word = wrd.lower()
+        if word in acronyms:
+            word_acr_info = acronyms[word]
+            word_index = word_acr_info[1]
+            # if there is no special index of word, then convert all words
+            words_acr_all = word_acr_info[0].split(" ")
+            if word_index == -1:
+                for words_acr_all_each in words_acr_all:
+                    result = result + " " + get_word_declension(morph, words_acr_all_each, 1)
+            else:
+                current_index = 0
+                for words_acr_all_each in words_acr_all:
+                    if current_index == word_index:
+                        result = result + " " + get_word_declension(morph, words_acr_all_each, declension_type)
+                    else:
+                        result = result + " " + words_acr_all_each
+                    current_index = current_index + 1
+        else:
+            result = result + " " + word
 
-	return result.strip()
+    return result.strip()
 
 
 def get_word_routines(morph, wrd, grm):
-	if morph is None:
-		print("Внутренняя ошибка. Морфоанализатор не создан.")
-		return ""
-	parsed = morph.parse(wrd)[0]
-	gent_text = parsed.inflect({grm})
-	if gent_text is not None:
-		return gent_text.word
-	else:
-		return wrd
+    if morph is None:
+        print("Внутренняя ошибка. Морфоанализатор не создан.")
+        return ""
+    parsed = morph.parse(wrd)[0]
+    gent_text = parsed.inflect({grm})
+    if gent_text is not None:
+        return gent_text.word
+    else:
+        return wrd
 
 
 def get_word_gent(morph, wrd):
-	return get_word_routines(morph, wrd, "gent")
+    return get_word_routines(morph, wrd, "gent")
 
 
 def get_word_ablt(morph, wrd):
-	return get_word_routines(morph, wrd, "ablt")
+    return get_word_routines(morph, wrd, "ablt")
 
 
 def get_word_datv(morph, wrd):
-	return get_word_routines(morph, wrd, "datv")
+    return get_word_routines(morph, wrd, "datv")
 
 
 def get_word_declension(morph, wrd, declension_type):
-	if declension_type == 0:
-		result = wrd
-	else:
-		if declension_type == 1:
-			result = get_word_gent(morph, wrd)
-		else:
-			if declension_type == 2:
-				result = get_word_ablt(morph, wrd)
-			else:
-				if declension_type == 3:
-					result = get_word_datv(morph, wrd)
-				else:
-					result = wrd
+    if declension_type == 0:
+        result = wrd
+    else:
+        if declension_type == 1:
+            result = get_word_gent(morph, wrd)
+        else:
+            if declension_type == 2:
+                result = get_word_ablt(morph, wrd)
+            else:
+                if declension_type == 3:
+                    result = get_word_datv(morph, wrd)
+                else:
+                    result = wrd
 
-	return result
+    return result
 
 
 def parse_date_str(date_str):
-	tokens = date_str.split(".")
+    tokens = date_str.split(".")
 
 
 # TODO
 
 def get_month_string(month_num):
-	months = ["января", "февраля", "марта", "апреля", "мая", "июня", "июля", "августа", "сентября", "октября",
-	          "ноября", "декабря"]
-	return months[month_num - 1]
+    months = ["января", "февраля", "марта", "апреля", "мая", "июня", "июля", "августа", "сентября", "октября",
+              "ноября", "декабря"]
+    return months[month_num - 1]
 
 
 # for a phrase contains several words
 def get_words_declension(morph, txt, declension_type):
-	tokens = str(txt).split(" ")
-	result = ""
-	for t in tokens:
-		result = result + get_word_declension(morph, t, declension_type) + " "
-	return result.rstrip()
+    tokens = str(txt).split(" ")
+    result = ""
+    for t in tokens:
+        result = result + get_word_declension(morph, t, declension_type) + " "
+    return result.rstrip()
 
 
 def get_date_str(dt):
-	if dt is None:
-		return ""
-	return f"{dt.day}.{dt.month}.{dt.year}"
+    if dt is None:
+        return ""
+    return f"{dt.day}.{dt.month}.{dt.year}"
 
 
 def get_date_str_format2(dt):
-	if dt is None:
-		return ""
-	yr = str(dt.year)
-	if len(yr) == 4:
-		yr = yr[2:]
-	return f"{dt.day}/{yr}"
+    if dt is None:
+        return ""
+    yr = str(dt.year)
+    if len(yr) == 4:
+        yr = yr[2:]
+    return f"{dt.day}/{yr}"
 
 
 def not_empty(s):
-	return s is not None and len(s) > 0
+    return s is not None and len(s) > 0
 
 
 def replace_with_glue(s):
-	return s.replace(" ", " ", 1)
+    return s.replace(" ", " ", 1)
 
 
 def glue_number_string(s):
-	tokens = s.split(" ")
-	if len(tokens) == 1:
-		return s
-	index = 1
-	result = tokens[index - 1]
-	while index < len(tokens):
-		divider = " "
-		if str(tokens[index - 1]).isnumeric() and not str(tokens[index]).isnumeric():
-			divider = " "
-		result = result + divider + tokens[index]
-		index = index + 1
-	return result
+    tokens = s.split(" ")
+    if len(tokens) == 1:
+        return s
+    index = 1
+    result = tokens[index - 1]
+    while index < len(tokens):
+        divider = " "
+        if str(tokens[index - 1]).isnumeric() and not str(tokens[index]).isnumeric():
+            divider = " "
+        result = result + divider + tokens[index]
+        index = index + 1
+    return result
 
 def capitalize_first_letter(s):
     if len(s) == 0:
